6538d7d44d7dc7af356b818125f6a66b073bb373
Ong Jiong Xun <e1123169@u.nus.edu>
2025-02-14 02:55:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import telebot
 from flask import Flask    
 from threading import Thread
-from alive import keep_alive    
+from flask import Flask    
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')    
+def home():    
+    return "Bot started! Running..."
+
+def run():    
+    app.run(host='0.0.0.0',port=8080)
+
+def keep_alive():    
+    t = Thread(target=run)    
+    t.start()
+
 keep_alive()
 
 # Replace 'YOUR_BOT_TOKEN' with your actual bot token from BotFather
